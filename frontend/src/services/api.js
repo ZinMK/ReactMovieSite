e1fd0e68934c1217c268b0065d4b3008afe8dd55
ZinMK <shanunapal@gmail.com>
2025-03-10 19:34:10 -0500

    deploy config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = "1e5c564db1c3bc92715b555b71a805f1";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getpopularMovies = async () => {
